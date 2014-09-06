b99036150514896c0eb493ea02e0d38f33fb9f28
bjohan <sa6bxq@gmail.com>
2014-09-07 00:05:18 +0200

    Added corbomite and started work with the PLL

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -4,12 +4,63 @@
 #include <string.h>
 #include <stdlib.h>
 #include <avr/pgmspace.h>
+#include "corbomite.h"
+#include "uart_command_lib.h"
+#include "ad4350.h"
 
+void testCommand()
+{
+	transmitStringP(PSTR("test command executed\r\n"));
+}
+
+void powerLed(uint8_t on)
+{
+	if(on)
+		PORTD |= _BV(PD3);
+	else
+		PORTD &= ~_BV(PD3);
+}
+
+DIG_OUT("pwr", powerLed, powerLedCmd);
+
+EVENT_OUT("cmd", testCommand, testCmd);
+
+INFORMATION_HINT("test hest", testHint);
+ 
+const EventData initEvent PROGMEM = {registeredEntries};
+
+const CorbomiteEntry init PROGMEM = 
+	{EVENT_OUT, internalId, (CorbomiteData*)&initEvent};
+
+const CorbomiteEntry last PROGMEM = {LASTTYPE, "", 0};
+
+
+
+const CorbomiteEntry * const entries[] PROGMEM = {
+	&testHint,
+	&testCmd,
+	&powerLedCmd,
+	&init, &last
+};
 
 
 int main(void) 
 {
+	initUsart();
+	transmitStringP(PSTR("CorboSpec initialized\r\n"));
+	initAD4350();
 	DDRD |= _BV(PD3);
 	PORTD |= _BV(PD3);
-	while(1);
+	while(1){
+	//initAD4350();
+		//sendWord(0);
+		if(readLockDetect())
+			PORTD |= _BV(PD3);
+		else
+			PORTD &= ~_BV(PD3);
+		/*PORTD =0; 
+		PORTD |= _BV(PD3);
+		PORTD =0;*/
+		commandLine();
+	}
 }
